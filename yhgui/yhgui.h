7bf6e21bfced08f083ded90f34e67f7fd8a68c4f
trarck <trarck@gmail.com>
2013-12-30 18:16:54 +0800

    [all]加入内部文件的引用

--- yhgui/yhgui.h
+++ yhgui/yhgui.h
@@ -4,7 +4,24 @@
 #include "YHGUIMacros.h"
 #include "Component.h"
 #include "Container.h"
+
+#include "event/UIEventNames.h"
+#include "event/UIEventListenerManager.h"
+
+#include "utils/Utils.h"
+
 #include "control/Button.h"
 #include "control/NormalButton.h"
+
+//交互项
+#include "interactive/Organizer.h"
+#include "interactive/DocumentTreeOrganizer.h"
+#include "interactive/ListOrganizer.h"
+#include "interactive/OrderedListOrganizer.h"
+#include "interactive/PriorityOrganizer.h"
+#include "interactive/IndependentDocumentTreeOrganizer.h"
+#include "interactive/IndependentListOrganizer.h"
+#include "interactive/IndependentOrderedListOrganizer.h"
+#include "interactive/IndependentPriorityOrganizer.h"
 
 #endif //COCOS_YHGUI_YHGUI_H_
